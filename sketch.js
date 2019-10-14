a79de807e81fbefdc43fd53e5911390e789c7ccc
chrischne <chrischne@gmail.com>
2019-10-14 19:52:08 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,13 @@
-'use strict';
-
-var tileCount = 40;
-var actRandomSeed = 0;
-
-var circleAlpha = 130;
-var circleColor;
-
 var temperature = 0;
-var easedTemperature = 0;
-var tempertureLabel = "T";
+var r = 0;
+
 
 
 function setup () {
-  createCanvas(windowWidth, windowHeight);
-  noFill();
-  circleColor = color(0, 0, 0, circleAlpha);
+  createCanvas(800, 600);
 
   var client = mqtt.connect('mqtt://aeba5ae7:98e21bb6bccdb957@broker.shiftr.io', {
-    clientId: 'vizinviz-circle-displace-mqtt'
+    clientId: 'vizinviz-simple-circle'
   });
   console.log('client', client);
 
@@ -26,79 +16,20 @@ function setup () {
     client.subscribe('/temperature');
   });
 
-  client.on('message', function(topic, message) {
+  client.on('message', function (topic, message) {
     console.log('new message:', topic, message.toString());
     temperature = +message.toString();
-  
+
   });
 }
 
 
-
 function draw () {
-
-  push();
-  translate(width / tileCount / 2, height / tileCount / 2);
-
   background(255);
 
-  randomSeed(actRandomSeed);
-
-  stroke(circleColor);
-  noFill();
-
-  easedTemperature = ease(easedTemperature, temperature);
-  var temperatureScale = map(easedTemperature, 20, 30, 0, 50);
-  temperatureScale = constrain(temperatureScale, 0, 100);
-
-  textSize(350);
-  textAlign(CENTER, CENTER);
-  textFont('Relevant');
-  noStroke();
-  fill('DeepPink');
-  textStyle(BOLD);
-  text(round(temperature) + '\u00B0', width / 2, height / 2);
-
-  for (var gridY = 0; gridY < tileCount; gridY++) {
-    for (var gridX = 0; gridX < tileCount; gridX++) {
-
-      var posX = width / tileCount * gridX;
-      var posY = height / tileCount * gridY;
-
-
-      //console.log(easedTemperature,temperature,temperatureScale);
-      //console.log('temperatureSCale',temperatureScale);
-
-      var shiftX = random(-temperatureScale, temperatureScale);
-      var shiftY = random(-temperatureScale, temperatureScale);
-
-      strokeWeight(3);
-      noFill();
-      stroke(0);
-      // ellipse(posX + shiftX, posY + shiftY, 20, 20);
-      noStroke();
-      fill(0);
-      // ellipse(posX,posY,3,3);
-      noFill();
-      stroke(0);
-      strokeWeight(2);
-      ellipse(posX + shiftX, posY + shiftY, 10, 10);
-
-      stroke(0);
-      strokeWeight(2);
-
-    }
-  }
-  pop();
-
-}
-
-function mousePressed () {
-  actRandomSeed = random(100000);
-}
-
-function keyReleased () {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  var targetR = map(temperature,20,30,0,300);
+  r = ease(r,targetR);
+  ellipse(width/2,height/2,r);
 }
 
 function ease (n, target) {
